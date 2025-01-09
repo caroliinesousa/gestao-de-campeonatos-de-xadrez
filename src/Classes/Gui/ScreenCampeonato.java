1c1419a02688e508c8944ecf122c1426e2e9b2ab
Vitor Ferreira <vitorferreira-20@outlook.com>
2025-01-09 22:17:48 +0000

    excluin

--- src/Classes/Gui/ScreenCampeonato.java
+++ src/Classes/Gui/ScreenCampeonato.java
@@ -131,9 +131,7 @@ public class ScreenCampeonato extends JFrame {
         fraseLabel2.setFont(new Font("Arial", Font.PLAIN, 12));
         JLabel fraseLabel3 = new JLabel("Selecione uma opção para começar a gestão!", SwingConstants.CENTER);
         fraseLabel3.setFont(new Font("Arial", Font.PLAIN, 12));
-
-
-        
+    
         JButton partidasSingularesButton = new JButton("Gerir Partidas Singulares");
         JButton partidasDuplasButton = new JButton("Gerir Partidas Duplas");
         JButton voltarApaginaInicialButton = new JButton("Voltar a Página Inicial");
@@ -146,5 +144,46 @@ public class ScreenCampeonato extends JFrame {
         setVisible(true);
 
     }
+
+    public void telaGerirTorneios(){
+        JFrame frame = new JFrame("Gerir Torneios");
+        JButton torneiosSingularesField;
+        JButton torneiosDuplasField;
+        JButton salvarButton;
+        JButton sairButton;
+        ControllerCampeonato gerirTorneios;
+
+        gerirTorneios = new ControllerCampeonato();
+
+        setTitle("Gestão de Campeonatos");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(800, 400);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(5, 1));
+        setLayout(new BorderLayout(75,75));
+
+        JPanel panel = new JPanel(new GridLayout(8, 1));
+        panel.setBorder(BorderFactory.createEmptyBorder(40,100,40,100));
+        JLabel fraseLabel = new JLabel("Gestão de Torneios", SwingConstants.CENTER);
+        fraseLabel.setFont(new Font("Arial", Font.BOLD, 28));
+        JLabel fraseLabel2 = new JLabel("Gerencie torneios com facilidade. Escolha uma das opções para começar!", SwingConstants.CENTER);
+        fraseLabel2.setFont(new Font("Arial", Font.PLAIN, 12));
+        JLabel fraseLabel3 = new JLabel("Selecione uma opção para começar a gestão!", SwingConstants.CENTER);
+        fraseLabel3.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JButton torneiosSingularesButton = new JButton("Gerir Torneios Singulares por pontos");
+        JButton torneiosDuplasButton = new JButton("Gerir Torneios Duplas");
+        JButton voltarApaginaInicialButton = new JButton("Voltar a Página Inicial");
+
+        panel.add(fraseLabel);
+        panel.add(fraseLabel2);
+        panel.add(torneiosSingularesButton);
+        panel.add(torneiosDuplasButton);
+        panel.add(voltarApaginaInicialButton);
+
+        add(panel);
+        setVisible(true);
    
+}
 }
